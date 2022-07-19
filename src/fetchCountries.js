7a6b11a25cd88247dbdabccf76cee10e1b88fb80
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2022-07-19 21:40:03 +0300

    3

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,11 @@
 
-
+    const BASE_URL = 'https://restcountries.com/v3.1/'
+    const ENDPOINT_NAME = 'name/'
 
 
 
 export function fetchCountries(name) {
     
-    BASE_URL = 'https://restcountries.com/v3.1/'
-    ENDPOINT_NAME = 'name/'
-
     return fetch(`${BASE_URL}${ENDPOINT_NAME}${name}`)
     .then(response => {
         if (response.status===404) throw error; else return response.json()})
